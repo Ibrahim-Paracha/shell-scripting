8e939bc224b9496340e258a99ef1cb9b202c56ac
Ibrahim-Paracha <a.aip.paracha@gmail.com>
2026-08-22 09:41:43 -0700

    HW

---
+++ homework/task-00/day-01-shell-scripting.sh
@@ -0,0 +1,5 @@
+## Name: Ibrahim Paracha
+## Date: 22/08/26
+## Linux Distro: Ubuntu (wsl)
+
+
--- homework/task-00/server_check.sh
+++ homework/task-00/server_check.sh
@@ -2,9 +2,9 @@
 
 service="nginx"
 
-read -r -p "Do you wish to check the status of $service? (y/n)" ans
+read -r -p "Do you wish to check the status of $service? (y/n)" ans 
 
-case "$ans" in
+case "$ans" in # case is like a drop down menu, you can choose the options to select an answer from
 y|Y)
 systemctl is-active --quiet "$service"
 ;;
@@ -13,7 +13,7 @@ n|N)
 echo "Skipped"
 ;;
 
-*)
+*) # This means that any other input other than the above will be put into this option
 echo "Invalid output" >&2
 exit 1
 ;;
